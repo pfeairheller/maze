0b1d5efcfe40ab7821d81d980a9236867d214609
Phil Feairheller <pfeairheller@gmail.com>
2015-12-08 12:54:47 -0500

    Updated shell file and gitignore

--- maze.sh
+++ maze.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 PWD=`pwd`
-export G3PO_ROOT=$PWD
-export G3PO_SOURCE=$G3PO_ROOT/src/github.com/g3po
+export MAZE_ROOT=$PWD
+export MAZE_SOURCE=$MAZE_ROOT/src/github.com/MAZE
 export GOPATH=$PWD:$PWD/vendor
 
 export PATH=$PWD/bin:$PWD/vendor/bin:$PATH
-export SLACK_BOT_TOKEN=xoxb-10899465207-JNXxFicSLMl7GkPO0PXSDRji
